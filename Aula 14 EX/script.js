8b4ae56f4fb70a0d3192f063a26e2a13b5d03998
DevDan88 <leitedaniel066@gmail.com>
2023-03-06 09:35:18 -0300

    Add files via upload

---
+++ Aula 14 EX/script.js
@@ -0,0 +1,26 @@
+function contar(){
+    let ini = document.getElementById('txti')
+    let fim = document.getElementById('txtf')
+    let pas = document.getElementById('txtp')
+    let res = document.getElementById('res')
+
+    if (ini.value.length == 0 || fim.value.length == 0||pas.value.length == 0){
+        window.alert('[ERRO] Faltam dados!')
+    }else{
+        res.innerHTML = '<strong>Contando: </strong>'
+        let i = Number(ini.value)
+        let f = Number(fim.value)
+        let p = Number(pas.value)
+
+        if(i < f){ // contagem crescente
+            for(let c = i; c <= f; c += p){
+                res.innerHTML += `${c} \u{1F449}`
+            }
+        }else{ // contagem regressiva
+            for( c = i; c >= f; c -= p){
+                res.innerHTML += `${c} \u{1F449}`
+            }
+        }
+        res.innerHTML += `\u{1F3C1}`
+    }
+}
